40346ee664c8c937625cac465e42e4681dd1d169
FRDHAL <frd.halassi@gmail.com>
2023-05-29 19:49:21 +0200

    Nouveau Nom

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ export function App() {
         <div className="row">
           <div className="col-4">
             <Logo
-              title="Watowatch"
+              title="Cine Best"
               subtitle="Find a show you may like"
               image={logoImg}
             />
